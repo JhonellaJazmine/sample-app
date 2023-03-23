d64752c4b7edeec734c93e90024d82aba9e6c476
JhonellaJazmine <orquillojhonella@gmail.com>
2023-03-23 22:33:43 +0800

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
 
+# BASIC LOGIN
+  get 'sessions/new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
   # Defines the root path route ("/")
